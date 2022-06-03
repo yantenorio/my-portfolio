334062546a07f3ff12b7f798b5994a9636268e2f
Yan <Yantenorio64@gmail.com>
2022-06-03 19:29:11 -0300

    its over

--- index.php
+++ index.php
@@ -5,39 +5,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <title>Yan Tenório</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<title>Yan Tenório</title>
 </head>
 
 <body>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">yantenorio</a>
+            <a class="navbar-brand" href="#">Portfolio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
-                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+    
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+    
+                </ul>
                 <span class="navbar-text">
-                    Desenvolvido por <a href="https://twitter.com">@yanxtenorio</a>
+                    Developed by <a href="https://twitter.com">yantenorio</a>
                 </span>
             </div>
         </div>
     </nav>
-
-    <div class="container">
+    
+      <div class="container">
         <div class="px-4 py-5 my-5 text-center">
             <img class="d-block mx-auto mb-4  rounded" src="" alt="" width="72">
             <h1 class="display-5 fw-bold">Yan Tenório</h1>
             <h4>Back-end Developer</h4>
+            <a href="https://github.com/yantenorio" class="link-dark">Github</a>
+            <a href="https://www.linkedin.com/in/yan-tenorio-20aa951b8/" class="link-dark">LinkedIn</a>
         </div>
 
         <div class="container mt-5 text-center">
             <div class="col-lg-6 mx-auto">
                 <h3>About Me</h3>
                 <p class="lead mb-4">
-                    Developer with three years studying about Computer Science and a IT degree who likes to code stuff
+                    Developer with three years studying about Computer Science who likes to code stuff
                     in PHP and
-                    Javascript.
+                    Javascript. My goal is to use my skills to collaborate with others and learn more about web development.
                 </p>
             </div>
         </div>
@@ -102,7 +112,10 @@
                             <div class="mx-3">
                                 <h4 class="card-title">Back-end Developer</h4>
                                 <h6>Metadevs</h6>
-                                <p class="card-text lead mb-4">At Metadevs I developed some of the main micro-applications that support the platform, where I used some interesting technologies such as: NodeJs, Firebase and Express. I also did the database(made in MongoDB) data modeling</p>
+                                <p class="card-text lead mb-4">At Metadevs I developed some of the main
+                                    micro-applications that support the platform, where I used some interesting
+                                    technologies such as: NodeJs, Firebase and Express. I also did the database (made in
+                                    MongoDB) data modeling.</p>
                                 <small><strong>2021-Current</strong></small>
                             </div>
                         </div>
@@ -115,45 +128,47 @@
         <br>
         <h3 class="mb-3">Contact</h3>
         <div class="container mt-5">
-            <form class="form-contact">
+            <form class="form-contact" action="main.php" method="post">
                 <div class="row">
                     <div class="col-6">
                         <div class="form-group">
                             <label for="inputName">Name</label>
-                            <input type="text" class="form-control" id="inputName" name="name"
-                                   placeholder="Your Name">
+                            <input type="text" class="form-control" id="inputName" name="name" placeholder="Your Name">
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group">
                             <label for="inputEmail">E-mail</label>
                             <input type="email" class="form-control" id="inputEmail" name="email"
-                                   placeholder="your@email.com">
+                                placeholder="your@email.com">
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputSubject">Subject</label>
                     <input type="text" class="form-control" id="inputSubject" name="subject"
-                           placeholder="I wish a freelance">
+                        placeholder="I wish a freelance">
                 </div>
 
                 <div class="form-group">
                     <label for="inputMessage">Message</label>
-                    <textarea class="form-control" id="inputMessage" name="content" rows="3"></textarea>
+                    <textarea class="form-control" id="inputMessage" name="msg" rows="3"></textarea>
                 </div>
                 <div class="text-right">
-                    <button type="button" class="btn btn-outline-dark">Send</button>
+                    <button type="submit" class="btn btn-outline-dark">Send</button>
                 </div>
             </form>
         </div>
 
-    <footer class="footer">
-        <p class="text-center mb-0 p-3">
-            <a href="https://github.com/yantenorio/yantenorio">Yan Tenório da Silva </a>© 2022
-        </p>
-    </footer>
-    <script src="./js/bootstrap.bundle.min.js"></script>
+
+
+
+        <footer class="footer">
+            <p class="text-center mb-0 p-3">
+                <a href="https://github.com/yantenorio/yantenorio" class="link-dark">Yan Tenório da Silva </a>© 2022
+            </p>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ main.php
@@ -0,0 +1,37 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$msg = $_POST['msg'];
+$send_date = date('d/m/Y');
+$send_time = date('H:i:s');
+
+
+$file = "
+    <html>
+    <p><b>Name: </b>$name</p>
+    <p><b>Email: </b>$email</p>
+    <p><b>Subject: </b>$subject</p>
+    <p><b>Message: </b>$msg</p>
+    <p><b>This email was sent at $send_time on $send_date</b></p>
+
+    </html>
+";
+
+
+$to = "tenorioyan11@gmail.com";
+
+
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+$headers[] = "From: $name <$email>";
+
+
+$success = mail($to, $subject, $msg, implode("\r\n", $headers));
+
+if (!$success) {
+    $errorMessage = error_get_last()['message'];
+} else {
+    header('location: index.php');
+}
